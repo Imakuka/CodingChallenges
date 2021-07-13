7c5c9ee12c19863bcc4a98fb0f7f4cd0a6ddd7dc
joe kuka <joekuka@gmail.com>
2021-07-13 12:50:32 -1000

    Player Paint & Expert Positioner

---
+++ ExpertPositioner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpertPositioner : MonoBehaviour
+{
+    /*Create a program that allows you to hit 1 one your keyboard and you spawn to position 1. Hit 2 and you spawn to position 2. Hit 3 and you spawn to position 3.*/
+    [SerializeField] private GameObject _player;
+    [SerializeField] private Transform _pos1;
+    [SerializeField] private Transform _pos2;
+    [SerializeField] private Transform _pos3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            transform.position = _pos1.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            transform.position = _pos2.position;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            transform.position = _pos3.position;
+        }
+    }
+}
---
+++ PlayerPaintMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaintMovement : MonoBehaviour
+{
+    [SerializeField] private int _speed = 3;
+    [SerializeField] private GameObject _paintDotPrefab;
+    [SerializeField] private Renderer _dotRend;
+    [SerializeField] private bool _isPlayerMoving = false;
+
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        StartCoroutine(PaintRoutine());
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+           _isPlayerMoving = true;
+
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            _isPlayerMoving = false;
+        }
+
+    }
+
+    private void PlayerMovement()
+    {
+
+        var horizontalMove = Input.GetAxis("Horizontal");
+        var verticalMove = Input.GetAxis("Vertical");
+        var direction = new Vector3(horizontalMove, 0, verticalMove);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+
+
+    }
+
+    private IEnumerator PaintRoutine()
+    {
+        if(_isPlayerMoving == true)
+        {
+            var newDrop = Instantiate(_paintDotPrefab);
+            newDrop.transform.position = transform.position;
+            newDrop.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
+            yield return new WaitForSeconds(1);
+        }
+
+    }
+}
