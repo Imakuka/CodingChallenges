01daf74c13c4ca35042de59be8aa9a51e212a6cc
joe kuka <joekuka@gmail.com>
2021-07-12 16:02:05 -1000

    Spawn Minion

---
+++ ManagerScripts/SpawnManagerMinion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerMinion : MonoBehaviour
+{
+    [SerializeField] private GameObject _minionPrefab;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_minionPrefab, transform);
+        }
+    }
+
+
+}
