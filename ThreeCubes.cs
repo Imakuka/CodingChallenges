214c2c9a1204bb484e2de05cdf2e93a25694187a
joe kuka <joekuka@gmail.com>
2021-07-12 16:16:22 -1000

    Three Cubes

---
+++ ThreeCubes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeCubes : MonoBehaviour
+{
+    [SerializeField] private Renderer[] _cubeRends;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for(int i = 0; i < _cubeRends.Length; i++)
+            {
+                foreach(var rend in _cubeRends)
+                {
+                    _cubeRends[i].material.color = Color.red;
+                }
+            }
+        }
+    }
+}
